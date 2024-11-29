3afd8ba3641e0ff948baf0dcd39dd00bbdd7a29e
Gaurav Sablok <gauravcodepro@gmail.com>
2024-11-29 22:14:08 +0100

    applicativesystem
    started to write this code and making a genome capture embedded approach.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rust-embedded-genome-view
- genome view with embedded rust
+ - genome view with embedded rust
+ - genome capture on specific regions.
+
+Gaurav Sablok
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod args;
+use args::AlignmentArgs;
+use colored::Colorize;
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+/*
+ *Author Gaurav Sablok
+ *Universitat Potsdam
+ *Date 2024-11-29
+
+* */
+
+fn main() {
+
+}
+
